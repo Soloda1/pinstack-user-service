86ca39c4605afaddca8b22fab61d04e1e54d64a0
Soloda1 <enderderder@gmail.com>
2025-05-02 00:35:17 +0400

    Add docker for migrator;

---
+++ migrations/000001_create_users_table_and_refresh_token_table.up.sql
@@ -0,0 +1,20 @@
+
+CREATE TABLE users (
+       id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+       username TEXT NOT NULL UNIQUE,
+       email TEXT NOT NULL UNIQUE,
+       password TEXT NOT NULL,
+       full_name TEXT,
+       bio TEXT,
+       avatar_url TEXT,
+       created_at TIMESTAMP DEFAULT now(),
+       updated_at TIMESTAMP DEFAULT now()
+);
+
+CREATE TABLE refresh_tokens (
+        id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        user_id BIGINT REFERENCES users(id) ON DELETE CASCADE,
+        token TEXT NOT NULL,
+        expires_at TIMESTAMP NOT NULL,
+        created_at TIMESTAMP DEFAULT now()
+);
